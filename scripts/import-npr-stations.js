c1b507966629abafd277e02505a6c72463c1a100
Cameron Sampson <csampson.js@gmail.com>
2016-05-23 22:15:12 -0500

    Fixed saving of imported npr station data

--- scripts/import-npr-stations.js
+++ scripts/import-npr-stations.js
@@ -24,32 +24,21 @@ if (!NPR_API_KEY) {
 const STATES = new Set(['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District Of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']);
 
 function raise(err) {
+  console.error(err);
   throw new Error(err);
 }
 
-function fetchStations(states) {
-  const operations = states.map(state => {
-    console.log(`Fetching stations for ${state}`);
+function fetchStations(state) {
+  console.log(`Fetching stations for ${state}`);
 
-    const endpoint = `http://api.npr.org/v2/stations/search/${state.toLowerCase()}?apiKey=${NPR_API_KEY}`;
-    return axios.get(endpoint).then(response => response.data);
-  });
+  const path     = encodeURIComponent(state);
+  const endpoint = `http://api.npr.org/v2/stations/search/${path}?apiKey=${NPR_API_KEY}`;
 
-  return Promise.all(operations);
+  return axios.get(endpoint).then(response => response.data);
 }
 
-function deserializeStations(states) {
-  const stations = states.reduce((prev, curr, index) => {
-    return curr.concat(prev); 
-  }, []);
-  
-  console.log(stations.filter(station => {
-    return station.urls.find(s => s.primary_stream === 1);
-  }).length);
-
-  return stations.filter(station => {
-    return station.urls.find(s => s.primary_stream === 1);
-  }).map(station => {
+function deserializeStations(stations) {
+  return stations.map(station => {
     const primaryStream = station.urls.find(s => s.primary_stream === 1);
 
     console.log(`Deserializing ${station.title}`);
@@ -81,10 +70,11 @@ function deserializeStations(states) {
 
 function fetchGeolocation(station) {
   const address = station.address.join('+');
+
   console.log(`Fetching geolocation for ${station.name}: ${address}`);
 
   return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${GOOGLE_API_KEY}`).then(response => {
-    const geometry = response.data.results[0].geometry;
+    const geometry = response.data.results[0] ? response.data.results[0].geometry : { location: {} };
 
     return Object.assign(station, {
       geolocation: { longitude: geometry.location.lng, latitude: geometry.location.lat }
@@ -101,26 +91,30 @@ function fetchGeolocations(stations) {
   return new Promise((resolve, reject) => {
     const fetcher = setInterval(() => {
       const iteration = iterator.next();
-      
+
       if (iteration.done) {
         resolve(Promise.all(operations));
         return clearInterval(fetcher);
       }
-      
+
       operations.push(fetchGeolocation(iteration.value));
       index++;
     }, 250);
   });
 }
 
-function saveStations(stations) {
-  console.log(stations.length);
-  fs.writeFileSync(path.join(__dirname, '../data/stations.json'), JSON.stringify(stations));
-  console.log('Completed saving station records to `../data/stations.json`');
+function saveStations(stations, state) {
+  const filename = state.toLowerCase().replace(' ', '-') + '.json';
+
+  fs.writeFileSync(path.join(__dirname, `../data/${filename}`), JSON.stringify(stations));
+  console.log(`Completed saving ${stations.length} station records to ../data/${filename}`);
 }
 
-fetchStations([...STATES])
-  .then(deserializeStations)
-  .then(fetchGeolocations)
-  .then(saveStations)
-  .catch(raise);
+[...STATES].reduce((prev, curr) => {
+  return prev.then(() => {
+    return fetchStations(curr)
+    .then(deserializeStations)
+    .then(fetchGeolocations)
+    .then(stations => saveStations(stations, curr))
+  }).catch(raise);
+}, Promise.resolve());
