18af716eb66bae127808253a340d14d9f4021feb
Cameron Sampson <csampson.js@gmail.com>
2016-06-12 18:22:20 -0500

    Added basic `GET /stations` route

--- routes.js
+++ routes.js
@@ -1,7 +1,15 @@
 'use strict';
 
-const Boom = require('boom');
-const Joi  = require('joi');
+const Boom     = require('boom');
+const Joi      = require('joi');
+const database = require('rethinkdb');
+
+// @todo Move this to `lib/database`
+let dbConnection;
+
+database.connect({ db: 'radio_api' })
+  .then(conn => { dbConnection = conn; })
+  .catch(err => { throw new Error(err); });
 
 module.exports = [
   {
@@ -9,6 +17,18 @@ module.exports = [
     path: '/',
     handler: (request, reply) => {
       reply();
+    }
+  },
+
+  {
+    method: 'GET',
+    path: '/stations',
+    handler: (request, reply) => {
+      database.table('stations').run(dbConnection, (err, cursor) => {
+        cursor.toArray((err, set) => {
+          reply(set);
+        });
+      });
     }
   }
 ];
