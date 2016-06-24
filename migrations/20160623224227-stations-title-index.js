24ea7e0463fd73c1d9adb95b3e28ec701706c5b6
Cameron Sampson <csampson.js@gmail.com>
2016-06-23 22:49:47 -0500

    Added `title` index to `stations` table

---
+++ migrations/20160623224227-stations-title-index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+function createIndex(database, connection) {
+  console.log('Adding `title` index to `stations` table...');
+
+  return new Promise((resolve, reject) => {
+    database.db('radio_api').table('stations').indexCreate('title').run(connection, results => {
+      console.log('...done');
+      resolve();
+    });
+  });
+}
+
+function deleteIndex(database, connection) {
+  console.log('Delete `title` index from `stations` table...');
+
+  return new Promise((resolve, reject) => {
+    database.db('radio_api').table('stations').indexDrop('title').run(connection, results => {
+      console.log('...done');
+      resolve();
+    });
+  });
+}
+
+function up(database, connection) {
+  return createIndex(database, connection)
+    .catch(err => { throw new Error(err); });
+}
+
+function down(database, connection) {
+  return deleteIndex(database, connection)
+    .catch(err => { throw new Error(err); });
+}
+
+module.exports.up   = up;
+module.exports.down = down;
