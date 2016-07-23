5cb96a72d2dea503a8c557ccace4b8b46cd6dd09
Cameron Sampson <csampson.js@gmail.com>
2016-07-23 15:26:22 -0500

    Added migration for removal of `area_code` and `phone_extension`

---
+++ migrations/20160723151509-remove-phone-subfields.js
@@ -0,0 +1,42 @@
+'use strict';
+
+function removeFields(database, connection) {
+  console.log('Removing fields `area_code` and `phone_extension` from `stations` table...');
+
+  return new Promise((resolve, reject) => {
+    database.db('radio_api')
+      .table('stations')
+      .replace(station => station.without('area_code', 'phone_extension'))
+      .run(connection, results => {
+        console.log('...done');
+        resolve();
+      });
+  });
+}
+
+function addFields(database, connection) {
+  console.log('Adding fields `area_code` and `phone_extension` to `stations` table...');
+
+  return new Promise((resolve, reject) => {
+    database.db('radio_api')
+      .table('stations')
+      .update({ area_code: null, phone_extension: null })
+      .run(connection, results => {
+        console.log('...done');
+        resolve();
+      });
+  });
+}
+
+function up(database, connection) {
+  return removeFields(database, connection)
+    .catch(err => { throw new Error(err); });
+}
+
+function down(database, connection) {
+  return addFields(database, connection)
+    .catch(err => { throw new Error(err); });
+}
+
+module.exports.up   = up;
+module.exports.down = down;
