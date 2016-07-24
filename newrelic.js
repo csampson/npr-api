dd25c676e74552352fe88a3fc8e0af0b18976cb1
Cameron Sampson <csampson.js@gmail.com>
2016-07-24 12:15:43 -0500

    Updated newrelic app name

--- newrelic.js
+++ newrelic.js
@@ -3,7 +3,7 @@ const environment = require('./lib/environment');
 'use strict';
 
 module.exports.config = {
-  app_name: ['public-radio-api-dev'],
+  app_name: ['public-radio-api'],
   license_key: environment.get('new_relic_key'),
   logging: {
     level: 'info'
