778ca74553db051e6be4317a1d0b5c05be473a1f
Cameron Sampson <csampson@users.noreply.github.com>
2017-02-17 18:34:44 -0600

    Data refresh + `filter` schema fix (#43)
    * Updated location files to include full streaming data
    
    * Static station data update + retroactively modified migration
    
    * Fixed `filter` schema syntax error

--- schemas/filter.js
+++ schemas/filter.js
@@ -3,20 +3,17 @@
 const Joi = require('joi');
 
 const filter = Joi.object().keys({
-    abbreviation: Joi.string(),
-    band: Joi.string(),
-    call: Joi.string(),
-    format: Joi.string(),
-    frequency: Joi.number(),
-    geolocation: Joi.array().items(Joi.number()),
-    market_city: Joi.string(),
-    market_state: Joi.string(),
-    name: Joi.string(),
-    tagline: Joi.string(),
-    title: Joi.string()
-    }),
-    sort: Joi.string(),
-    page: Joi.string()
-}
+  abbreviation: Joi.string(),
+  band: Joi.string(),
+  call: Joi.string(),
+  format: Joi.string(),
+  frequency: Joi.number(),
+  geolocation: Joi.array().items(Joi.number()),
+  market_city: Joi.string(),
+  market_state: Joi.string(),
+  name: Joi.string(),
+  tagline: Joi.string(),
+  title: Joi.string()
+});
 
 module.exports = filter;
