3f6ca59ebb9c6fba5a21c542b9b9ddba88c964a9
Cameron <csampson.js@gmail.com>
2018-03-09 22:18:19 -0600

    Apollo Engine integration

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const KoaRouter = require('koa-router')
 const koaLogger = require('koa-bunyan-logger')
 const koaBody = require('koa-bodyparser')
 const { graphqlKoa, graphiqlKoa } = require('apollo-server-koa')
+const { ApolloEngine } = require('apollo-engine')
 
 const schema = require('./graphql/schema')
 
@@ -12,14 +13,33 @@ const app = new Koa()
 const router = new KoaRouter()
 const PORT = 3000
 
+if (!process.env.APOLLO_ENGINE_KEY) {
+  throw new Error('Missing required environment variable: APOLLO_ENGINE_KEY')
+}
+
+const engine = new ApolloEngine({
+  apiKey: process.env.APOLLO_ENGINE_KEY,
+  logging: {
+    level: 'INFO' // Engine Proxy logging level. DEBUG, INFO (default), WARN or ERROR.
+  }
+})
+
+router.post('/graphql', graphqlKoa({
+  tracing: true,
+  cacheControl: true,
+  schema
+}))
+
+router.get('/graphiql', graphiqlKoa({ endpointURL: '/graphql' }))
+
 app.use(koaBody())
 app.use(koaLogger())
 app.use(koaLogger.requestIdContext())
 app.use(koaLogger.requestLogger())
-
-router.post('/graphql', graphqlKoa({ schema }))
-router.get('/graphiql', graphiqlKoa({ endpointURL: '/graphql' }))
-
 app.use(router.routes())
 app.use(router.allowedMethods())
-app.listen(PORT)
+
+engine.listen({
+  port: PORT,
+  koaApp: app
+})
